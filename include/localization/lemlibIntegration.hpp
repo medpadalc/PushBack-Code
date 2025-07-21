ba981eab0f191799fe01cfb0c675f27a212a5a1b
TheMaster3558 <chawk_k@outlook.com>
2025-07-20 21:59:57 -0700

    Use normal distribution

--- include/localization/lemlibIntegration.hpp
+++ include/localization/lemlibIntegration.hpp
@@ -36,20 +36,17 @@ public:
         const lemlib::Pose after = getPose(true, true);
         const lemlib::Pose change = after - before;
 
-        std::uniform_real_distribution<> xDistribution(change.x - DRIVE_NOISE * std::fabs(change.x),
-                                                          change.x + DRIVE_NOISE * std::fabs(change.x));
-        std::uniform_real_distribution<> yDistribution(change.y - DRIVE_NOISE * std::fabs(change.y),
-                                                    change.y + DRIVE_NOISE * std::fabs(change.y));
-        std::uniform_real_distribution angleDistribution(change.theta - ANGLE_NOISE * std::fabs(change.theta),
-                                                          change.theta + ANGLE_NOISE * std::fabs(change.theta));
+        std::normal_distribution<> xDistribution(0, DRIVE_NOISE * std::fabs(change.x) + 0.001);
+        std::normal_distribution<> yDistribution(0, DRIVE_NOISE * std::fabs(change.y) + 0.001);
+        std::normal_distribution<> angleDistribution(0, ANGLE_NOISE * std::fabs(change.theta) + 0.001);
 
         static auto& randomGen = pf.getRandomGen();
 
         // No risk of dangling reference
-        pf.update([&xDistribution, &yDistribution, &angleDistribution]() {
-            const auto noisyX = xDistribution(randomGen);
-            const auto noisyY = yDistribution(randomGen);
-            const auto noisyTheta = angleDistribution(randomGen);
+        pf.update([&xDistribution, &yDistribution, &angleDistribution, &change]() {
+            const auto noisyX = change.x + xDistribution(randomGen);
+            const auto noisyY = change.y + yDistribution(randomGen);
+            const auto noisyTheta = change.theta + angleDistribution(randomGen);
 
             return Eigen::Vector3f(noisyX, noisyY, noisyTheta);
         });
