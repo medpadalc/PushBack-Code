c03644bddc8401faf73e57346f865a1c6d2e1dc0
Kaushal Dabbiru <92063273+TheMaster3558@users.noreply.github.com>
2025-11-13 11:03:54 -0800

    Implement example function for intake and hood control
    Added example function to demonstrate intake and hood operations.

--- src/autons.cpp
+++ src/autons.cpp
@@ -1,5 +1,28 @@
 #include "autons.hpp"
 
+void example() {
+    // long goal
+    subsystems::intake::run(subsystems::intake::GoalType::LONG_GOAL);
+    pros::delay(1000);
+    subsystems::intake::stop();
+
+    // medium goal
+    subsystems::intake::run(subsystems::intake::GoalType::MEDIUM_GOAL);
+    pros::delay(1000);
+    subsystems::intake::stop();
+
+    // low goal
+    subsystems::intake::run(subsystems::intake::GoalType::LOW_GOAL);
+    pros::delay(1000);
+    subsystems::intake::stop();
+
+    subsystems::hood::open();
+    subsystems::hood::close();
+
+    subsystems::matchload::extend();
+    subsystems::matchload::retract();
+}
+
 void solo_awp() {
 }
 
